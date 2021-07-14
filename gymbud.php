90f4d959534332082865373ca8d0fc194367f81f
Neil Daly <neil.daly@wpengine.com>
2021-07-14 11:26:09 +0100

    feat: :sparkles: add plugin meta info

---
+++ gymbud.php
@@ -0,0 +1,12 @@
+<?php
+/*
+Plugin Name:  GymBud
+Description:  Easily generate informative posts for exercise descriptions tailored to your workout needs.
+Plugin URI:   https://neildalydev.com
+Author:       Neil Daly
+Version:      1.0
+Text Domain:  gymbud
+Domain Path:  /languages
+License:      GPL v2 or later
+License URI:  https://www.gnu.org/licenses/gpl-2.0.txt
+*/
